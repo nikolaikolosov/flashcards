7b88046e57a713b9dc2e746b44feba45b8c61d67
nikolay <nrf-224@mail.ru>
2017-03-21 20:58:29 +0300

    Third task

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,53 @@
 class CardsController < ApplicationController
+  before_action :set_card, only: [:edit, :update, :destroy]
+
   def index
     @cards = Card.all
+  end
+
+  def new
+    @card = Card.new
+  end
+
+  def edit
+  end
+
+  def create
+    @card = Card.new(card_params)
+
+    respond_to do |format|
+      if @card.save
+        format.html { redirect_to action: :index }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @card.update(card_params)
+        format.html { redirect_to cards_url }
+      else
+        format.html { render :edit }
+      end
+    end
+end
+
+  def destroy
+    @card.destroy
+    respond_to do |format|
+      format.html { redirect_to cards_url }
+    end
+end
+
+  private
+
+  def set_card
+    @card = Card.find(params[:id])
+  end
+
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text)
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,22 @@
 class Card < ApplicationRecord
+  validates :original_text, :translated_text, presence: true
+  validate :different_text
+  before_validation :set_review_date_plus_three_days, on: :create
+  
+  private
+
+  def set_review_date_plus_three_days
+    self.review_date = Time.now + 3.days
+  end
+
+  def different_text
+    if trim_downcase(original_text) == trim_downcase(translated_text)
+      errors.add(:original_text, 'Вводимые значения должны отличаться.')
+    end
+  end
+
+    def trim_downcase(str)
+    str.mb_chars.downcase.to_s.squeeze(' ').lstrip
+  end
+
 end
