7049c740a674582c350217b827d4dd16eb6eed98
nikolay <nrf-224@mail.ru>
2017-03-24 22:45:29 +0300

    Fifth task

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,23 @@
 class PagesController < ApplicationController
   def index
+    @card = Card.review.first
+  end
+
+  def check_card
+    @card = Card.find(params[:card_id])
+    check_result = @card.check_answer(answer_params[:answer], answer_params[:translated_text])
+    if check_result
+      @card.review_date = 3.days.from_now
+      @card.save
+    else
+      flash[:notice] = 'Incorrect'
+    end
+    redirect_to root_path
+  end
+
+  private
+  
+  def answer_params
+    params.permit(:answer, :translated_text)
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -2,7 +2,15 @@ class Card < ApplicationRecord
   validates :original_text, :translated_text, presence: true
   validate :different_text
   before_validation :set_review_date, on: :create
+
+  scope :review, -> { where("review_date <= ?", Time.now).order('RANDOM()')  }
   
+  def check_answer(answer, translated_text)
+    if trim_downcase(answer) == trim_downcase(translated_text)
+      return true
+    end
+  end
+
   private
 
   def set_review_date
