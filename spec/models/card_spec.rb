ec3e237c38ba650c1aa971ff4d42acd5205f01bf
nikolay <nrf-224@mail.ru>
2017-03-28 19:29:27 +0300

    Fixes in sixth task

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,27 +2,27 @@ require 'rails_helper'
 require 'support/factory_girl'
   
 RSpec.describe Card, type: :model do
-  context 'validations' do 
-    it 'create card with empty original text' do
-      card = build(:card, original_text: '')
-      expect(card).to be_invalid
-    end
-
-    it 'create card with empty translated text' do
-      card = build(:card, translated_text: '')
-      expect(card).to be_invalid
-    end
-
-    it 'create card with same original and translated text' do
-      card = build(:card, original_text: 'Stadt', translated_text: 'stadt')
-      expect(card).to be_invalid
-    end
-  end
-
   context 'callbacks' do   
     it 'get correct review date on create' do
       card = create(:card)
-      expect(card.review_date.strftime('%Y-%m-%d %H:%M')).to eq(Time.zone.now.strftime('%Y-%m-%d %H:%M'))
+      expect(card.review_date.to_date).to eq(3.days.from_now.to_date)
+    end
+
+    it 'get incorrect review date on create' do
+      card = create(:card)
+      expect(card.review_date.to_date).not_to eq(Time.now.to_date)
+    end
+
+    it 'test check_answer method as true' do
+      card = create(:card)
+      check_result = card.check_answer('city')
+      expect(check_result).to be true
+    end
+
+    it 'test check_answer method as false' do
+      card = create(:card)
+      check_result = card.check_answer('village')
+      expect(check_result).to be false
     end
   end
 end
