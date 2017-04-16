9035c603e1f489f4be7be786b85f0290dbcdfcb3
nikolay <nrf-224@mail.ru>
2017-04-16 21:53:18 +0300

    Twelfth task

--- db/migrate/20170407192738_add_image_to_cards.rb
+++
@@ -1,5 +0,0 @@
-class AddImageToCards < ActiveRecord::Migration[5.0]
-  def change
-    add_column :cards, :image, :string
-  end
-end
--- db/migrate/20170411192759_add_pack_id_to_cards.rb
+++
@@ -1,5 +0,0 @@
-class AddPackIdToCards < ActiveRecord::Migration[5.0]
-  def change
-    add_column :cards, :pack_id, :integer
-  end
-end
